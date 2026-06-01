04f12de90b1fb593a6e6d483ce985df824a8b054
Maks <9temashuptar9@gmail.com>
2026-06-01 14:30:39 +0300

    Hom work3.3

---
+++ 03/DZ_3.3.py
@@ -0,0 +1,21 @@
+number = [1, 2, 3, 4, 5, 6]
+number_st = (len(number) // 2)
+
+print(number[:number_st], number[number_st:])
+
+
+number2 = [1, 2, 3]
+number_mid = ((len(number2)+1) // 2)
+
+print(number2[:number_mid], number2[number_mid:])
+
+
+number3 =  [1,]
+num = (len(number3) // 2)
+
+print(number3[num:], number3[:num])
+
+number4 = []
+nam2 = (len(number4) //2)
+
+print(number4[:], number4[:])
